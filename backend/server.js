fb935707f2e1406ad6f1e49eaa278957af49f6e4
manos-grigorakis <manos.grigorakis03@gmail.com>
2024-07-02 22:12:14 +0300

    feat: Added basic structure of backend and created a connection with the database

---
+++ backend/server.js
@@ -0,0 +1,32 @@
+require('dotenv').config()
+
+const express = require('express')
+const mongoose = require('mongoose')
+
+const app = express()
+const port = 3000
+
+
+// Connection with database
+mongoose.connect(process.env.MONGODB_URI, {
+}).then(() => {
+    console.log('Connected to MongoDB Atlas');
+}).catch((error) => {
+    console.error('Error connecting to MongoDB Atlas', error);
+});
+
+
+// Test route!
+app.get('/test', (req, res) => {
+    res.send('Connected Successfully')
+    console.log('Connected Successfully')
+})
+
+app.listen(port, () => {
+    console.log(`Server listening on: http://localhost:${port}`)
+})
+
+// MongoDB Atlas Credentials
+// username: manosgrigorakis03
+// password: hVs52KkcpFvV4ZqC
+
