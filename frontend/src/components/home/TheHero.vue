930e531469cea4606438f62e7dacc2ac92bac07e
manos-grigo <manos.grigorakis03@gmail.com>
2024-08-20 04:55:33 +0300

    feat: Added animations on home screen and on forms.

--- frontend/src/components/home/TheHero.vue
+++ frontend/src/components/home/TheHero.vue
@@ -5,7 +5,7 @@
       class="absolute bg-hero-image bg-center bg-no-repeat bg-cover w-full h-full top-0 pointer-events-none z-0"
     ></div>
     <div class="flex flex-col items-start gap-10 md:gap-20 relative z-10 px-4 lg:px-32">
-      <div class="flex flex-col gap-4 text-white">
+      <div class="flex flex-col gap-4 text-white" ref="heroText">
         <h1 class="text-4xl md:text-6xl font-semibold">
           Find Your <span class="text-vibrantOrange-default font-bold">Perfect</span> Ride
         </h1>
@@ -13,11 +13,27 @@
           From economy to luxury, choose from a wide selection of top-quality vehicles.
         </p>
       </div>
-      <HeroForm />
+      <HeroForm ref="heroForm" />
     </div>
   </section>
 </template>
 
 <script setup>
+import { ref, onMounted } from 'vue'
+import { gsap } from 'gsap'
+
 import HeroForm from '@/components/home/HeroForm.vue'
+
+const heroText = ref(null)
+const heroForm = ref(null)
+
+onMounted(() => {
+  gsap.from(heroText.value, { x: -400, duration: 1.2 })
+  gsap.from(heroForm.value.$el, { x: 800, duration: 1.4 })
+
+  // gsap.from(heroText.value, { y: -300, duration: 1.5 })
+  // gsap.from(heroForm.value.$el, { y: 250, duration: 1.5 })
+
+  // gsap.from(heroText.value, { scale: 0, duration: 1.5 })
+})
 </script>
--- frontend/src/components/ui/FormWrapper.vue
+++ frontend/src/components/ui/FormWrapper.vue
@@ -1,7 +1,19 @@
 <template>
   <div
+    ref="formWrapper"
     class="flex min-h-full flex-1 flex-col justify-center px-6 py-12 mt-16 lg:px-8 bg-white drop-shadow-md rounded-lg max-w-sm md:max-w-lg mx-auto"
   >
     <slot></slot>
   </div>
 </template>
+
+<script setup>
+import { ref, onMounted } from 'vue'
+import { gsap } from 'gsap'
+
+const formWrapper = ref(null)
+
+onMounted(() => {
+  gsap.from(formWrapper.value, { scale: 0.7, duration: 0.7 })
+})
+</script>
