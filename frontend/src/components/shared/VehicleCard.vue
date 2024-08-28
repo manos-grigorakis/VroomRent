a2c2bd462e0d99b366af23cfd4034ebfae25d494
manos-grigo <manos.grigorakis03@gmail.com>
2024-08-28 14:36:23 +0300

    .

--- frontend/src/components/shared/VehicleCard.vue
+++ frontend/src/components/shared/VehicleCard.vue
@@ -1,8 +1,6 @@
 <template>
   <!-- Card -->
-  <div
-    class="relative bg-white max-w-sm drop-shadow-md rounded-md pt-14 pb-8 mx-auto h-full max-h-[480px]"
-  >
+  <div class="relative bg-white max-w-sm drop-shadow-md rounded-md pt-14 pb-8 mx-auto">
     <VehiclePremiumTag v-if="vehicle.premiumDelivery" />
     <VehicleDetails
       :image="vehicle.image"
