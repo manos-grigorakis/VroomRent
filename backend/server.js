114278339b676fe4bf3e0f8867221de4d42d9bc1
manos-grigo <manos.grigorakis03@gmail.com>
2025-01-29 18:45:46 +0200

    Updated Cors settings to support multiple origins.

--- backend/server.js
+++ backend/server.js
@@ -8,8 +8,16 @@ const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+const allowedOrigins = process.env.CORS_ORIGIN.split(",");
+
 const corsOptions = {
-  origin: process.env.CORS_ORIGIN,
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
   optionsSuccessStatus: 200,
 };
 
